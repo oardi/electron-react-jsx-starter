471f3680daa0e39c01231fb1ce6ee357d449aece
Ardian Shala <ardian.sh@gmx.de>
2020-01-09 19:21:41 +0100

    init

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+	app.quit();
+}
+
+let mainWindow;
+
+const createWindow = () => {
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+	});
+
+	mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+
+	mainWindow.webContents.openDevTools();
+
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
---
+++ webpack.renderer.config.js
@@ -0,0 +1,45 @@
+
+const rules = require('./webpack.rules');
+
+rules.push({
+	test: /\.(js|jsx)$/,
+	exclude: /node_modules/,
+	loader: ['babel-loader']
+});
+
+rules.push({
+	test: /\.css$/,
+	use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+rules.push({
+	test: /\.s[ac]ss$/i,
+	use: [
+		'style-loader',
+		'css-loader',
+		'sass-loader',
+	],
+})
+
+rules.push({ test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader?name=assets/[name].[ext]' })
+
+rules.push({
+	test: /\.(png|jpg|gif)$/,
+	use: [
+		{
+			loader: 'file-loader',
+			options: {
+				name: '[name].[ext]',
+				outputPath: 'assets/img',
+				publicPath: 'assets/img'
+			}
+		}
+	]
+})
+
+module.exports = {
+	// Put your normal webpack config below here
+	module: {
+		rules,
+	},
+};
