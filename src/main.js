57bc3b99540d50aa47880558f655dec5a6bc8d14
Ardian Shala <ardian.sh@outlook.de>
2020-09-21 07:42:00 +0200

    updated packages

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const createWindow = () => {
 
 	mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
-	mainWindow.webContents.openDevTools();
+	// mainWindow.webContents.openDevTools();
 
 	mainWindow.on('closed', () => {
 		mainWindow = null;
